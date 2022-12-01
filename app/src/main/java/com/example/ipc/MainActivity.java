28352428832db2e3ebe96fe174e86346c92e131a
yn030727 <2601098388@qq.com>
2022-12-01 14:31:18 +0800

    1.序列化和反序列化

--- app/src/main/java/com/example/ipc/MainActivity.java
+++ app/src/main/java/com/example/ipc/MainActivity.java
@@ -4,12 +4,42 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectInputValidation;
+import java.io.ObjectOutputStream;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        User user = new User(0 , "jake" ,true);
 
+        //序列化过程
+        try {
+            ObjectOutputStream out = new ObjectOutputStream(new FileOutputStream("cache.txt"));
+            out.writeObject(user);
+            out.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        //反序列化过程
+        ObjectInputStream input = null;
+        try {
+            input = new ObjectInputStream(new FileInputStream("cache.txt"));
+            User newUser = (User)input.readObject();
+            input.close();
+        } catch (IOException | ClassNotFoundException e) {
+            e.printStackTrace();
+        }
+
+        //序列化过程将元素添加到文件当中
+        //反序列化过程将元素从文件当中读取出来
+        //上述代码演示了采用
     }
 }
--- app/src/main/java/com/example/ipc/User.java
+++ app/src/main/java/com/example/ipc/User.java
@@ -2,10 +2,31 @@ package com.example.ipc;
 
 import java.io.Serializable;
 
+
 public class User implements Serializable {
     //只需要实现Serializable接口即可
     private static final long serialVersionUID = 519067123721295773L;
+    //声明serialVersionUID，不是必须的，但是如果不声明，会影响反序列化
     public int userId;
     public String userName;
     public boolean isMale;
+
+    public User(int userId, String userName, boolean isMale) {
+        this.isMale=isMale;
+        this.userId=userId;
+        this.userName=userName;
+    }
+
+    public int getUserId() {
+        return userId;
+    }
+
+    public static long getSerialVersionUID() {
+        return serialVersionUID;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
 }
